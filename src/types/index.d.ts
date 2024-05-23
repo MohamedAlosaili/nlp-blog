a2acd1dfd1b5251d624bf8bdc2873da9bb71dd5a
MohamedAlosaili <mohamed.alosaili@gmail.com>
2024-05-23 17:53:39 +0300

    types: add login/signup & errorCode types

---
+++ src/types/index.d.ts
@@ -0,0 +1,25 @@
+export interface ActionReturn<T = any> {
+  errorCode?: ErrorCode;
+  data?: T;
+}
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface SignupData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export type ErrorCode =
+  | "internal_server_error"
+  | "invalid_credentials"
+  | "duplicate_email"
+  | "invalid_email"
+  | "invalid_name"
+  | "invalid_password";
+
+export type CookieName = "session";
