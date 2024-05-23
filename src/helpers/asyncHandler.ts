1129c88722e57be8df376a8300197b6f2bf939e9
MohamedAlosaili <mohamed.alosaili@gmail.com>
2024-05-23 17:56:12 +0300

    feat: add async wrapper to handle unexpected errors

---
+++ src/helpers/asyncHandler.ts
@@ -0,0 +1,13 @@
+import { ActionReturn } from "../types";
+
+export const asyncHandler = async <T>(
+  fn: () => Promise<ActionReturn<T>>
+): Promise<ActionReturn<T>> => {
+  try {
+    const result = await fn();
+
+    return result;
+  } catch (error) {
+    return { errorCode: "internal_server_error" };
+  }
+};
