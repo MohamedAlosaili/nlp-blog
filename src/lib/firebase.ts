557b9f9e9e26e2533d1a2915df1aca23c6877d0d
MohamedAlosaili <mohamed.alosaili@gmail.com>
2024-06-05 20:52:50 +0300

    fix firebase secret keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,19 @@ import "server-only";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
 
-const serviceAccount = require("@/../firebaseServiceAccountKey.json") as Record<
-  string,
-  string
->;
+const firebaseConfig = {
+  project_id: process.env.PROJECT_ID!,
+  private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n") ?? "", // Handle line breaks
+  client_email: process.env.CLIENT_EMAIL!,
+};
 
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccount),
+    credential: cert({
+      clientEmail: firebaseConfig.client_email,
+      privateKey: firebaseConfig.private_key,
+      projectId: firebaseConfig.project_id,
+    }),
     storageBucket: "gs://nlp-blog-adb56.appspot.com",
   });
 }
